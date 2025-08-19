edfac3aa575433bb70f5c690e63acd922df696ea
greatdignity <emaiwoolabipupo@gmail.com>
2025-08-19 13:37:56 +0100

    task_4

---
+++ task_4.sql
@@ -0,0 +1,16 @@
+-- task_4.sql
+-- Script to print the full description of the 'books' table
+-- without using DESCRIBE or EXPLAIN
+
+USE alx_book_store;
+
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'books';
